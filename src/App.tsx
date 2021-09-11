44f73aab8de8bcc412ada8800809f809924f5b7b
pessolatohenrique <pessolatohenrique@gmail.com>
2021-09-11 15:28:34 -0300

    Busca de usuarios com base no TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+interface IUser {
+  name: string;
+  email: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    async function searchUsers() {
+      const response = await axios.get<IUser[]>("http://localhost:3000/users");
+      setUsers(response.data);
+    }
+
+    searchUsers();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {[...users].map((item) => (
+        <p key={item.email}>{item.name}</p>
+      ))}
     </div>
   );
 }
